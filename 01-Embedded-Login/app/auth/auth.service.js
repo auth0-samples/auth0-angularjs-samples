15d6ec64fc60a92c3aad7b2d55306cbe88a6efee
Ryan Chenkie <ryanchenkie@gmail.com>
2017-07-14 15:14:23 -0400

    formatting

--- 01-Embedded-Login/app/auth/auth.service.js
+++ 01-Embedded-Login/app/auth/auth.service.js
@@ -1,23 +1,19 @@
-(function () {
-
+(function() {
   'use strict';
 
-  angular
-    .module('app')
-    .service('authService', authService);
+  angular.module('app').service('authService', authService);
 
   authService.$inject = ['$state', 'lock'];
 
   function authService($state, lock) {
-
     function login() {
       lock.show();
     }
-    
+
     function handleAuthentication() {
       // uncomment if you are not using HTML5Mode
       // lock.interceptHash();
-      
+
       lock.on('authenticated', function(authResult) {
         if (authResult && authResult.accessToken && authResult.idToken) {
           setSession(authResult);
@@ -27,13 +23,17 @@
       lock.on('authorization_error', function(err) {
         $state.go('home');
         console.log(err);
-        alert('Error: ' + err.error + '. Check the console for further details.');
+        alert(
+          'Error: ' + err.error + '. Check the console for further details.'
+        );
       });
     }
 
     function setSession(authResult) {
       // Set the time that the access token will expire at
-      let expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+      let expiresAt = JSON.stringify(
+        authResult.expiresIn * 1000 + new Date().getTime()
+      );
       localStorage.setItem('access_token', authResult.accessToken);
       localStorage.setItem('id_token', authResult.idToken);
       localStorage.setItem('expires_at', expiresAt);
@@ -46,9 +46,9 @@
       localStorage.removeItem('expires_at');
       $state.go('home');
     }
-    
+
     function isAuthenticated() {
-      // Check whether the current time is past the 
+      // Check whether the current time is past the
       // access token's expiry time
       let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
       return new Date().getTime() < expiresAt;
@@ -59,6 +59,6 @@
       handleAuthentication: handleAuthentication,
       logout: logout,
       isAuthenticated: isAuthenticated
-    }
+    };
   }
 })();
